6f3801a325f0d35efed2edca3ed7027824a8fe4a
Pgl8 <pepe.gl@gmail.com>
2019-01-19 18:40:38 +0100

    Updated fixtures.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -13,11 +14,22 @@ class AppFixtures extends Fixture {
      * @throws \Exception
      */
     public function load(ObjectManager $manager) {
+        $this->loadBlogPosts($manager);
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @throws \Exception
+     */
+    public function loadBlogPosts(ObjectManager $manager) {
+        // Get the reference
+        $user = $this->getReference('user_admin');
+
         $blogPost = new BlogPost();
         $blogPost->setTitle('A new post from fixtures');
         $blogPost->setPublished(new \DateTime('2019-01-19 14:00:00'));
         $blogPost->setContent('Lorem  ipsum');
-        $blogPost->setAuthor('Admin');
+        $blogPost->setAuthor($user);
         $blogPost->setSlug('fixtures-post');
         $manager->persist($blogPost);
 
@@ -25,11 +37,37 @@ class AppFixtures extends Fixture {
         $blogPost->setTitle('A new post from fixtures 2');
         $blogPost->setPublished(new \DateTime('2019-01-19 14:00:00'));
         $blogPost->setContent('Lorem  ipsum2');
-        $blogPost->setAuthor('Admin');
+        $blogPost->setAuthor($user);
         $blogPost->setSlug('fixtures-post-2');
 
         $manager->persist($blogPost);
 
         $manager->flush();
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function loadUsers(ObjectManager $manager) {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@example.com');
+        $user->setName('John Doe');
+
+        // TODO: Do this properly
+        $user->setPassword('admin1234$$');
+
+        // Add reference
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function loadComments(ObjectManager $manager) {
+
     }
 }
