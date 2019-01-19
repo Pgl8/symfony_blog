49530e5821509b5d9f2f10d1091f3599c07d9134
Pgl8 <pepe.gl@gmail.com>
2019-01-19 14:23:21 +0100

    Send data fixtures to DB.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\BlogPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
-{
-    public function load(ObjectManager $manager)
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+class AppFixtures extends Fixture {
+    /**
+     * Load data fixtures with the passed EntityManager
+     * @param ObjectManager $manager
+     * @throws \Exception
+     */
+    public function load(ObjectManager $manager) {
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A new post from fixtures');
+        $blogPost->setPublished(new \DateTime('2019-01-19 14:00:00'));
+        $blogPost->setContent('Lorem  ipsum');
+        $blogPost->setAuthor('Admin');
+        $blogPost->setSlug('fixtures-post');
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A new post from fixtures 2');
+        $blogPost->setPublished(new \DateTime('2019-01-19 14:00:00'));
+        $blogPost->setContent('Lorem  ipsum2');
+        $blogPost->setAuthor('Admin');
+        $blogPost->setSlug('fixtures-post-2');
+
+        $manager->persist($blogPost);
 
         $manager->flush();
     }
